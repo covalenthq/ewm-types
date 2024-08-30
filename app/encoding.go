e498f50036c9383fe46ade8fe1732b87587f5e9b
Pranay Valson <pranay.valson@gmail.com>
2024-08-30 10:51:16 -0700

    go mod tidy for go-1.22.5; fix package import
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- app/encoding.go
+++ app/encoding.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 
+	"github.com/covalenthq/ewm-types/app/params"
+
 )
 
 // makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
